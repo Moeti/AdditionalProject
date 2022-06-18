680fd25fd91279bed0687cabede9621d26e886b4
MoetiK <moeti.khetsi@gmail.com>
2022-06-18 12:08:46 +0200

    Added changes to campingList

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
+    clickCreateItem  : function(component, event, helper) {
+    var validItem = component.find('newcampingform').reduce(function(validSoFar, inputCmp){
+        // Show error messages for invalid fields
+        inputCmp.showHelpMessageIfInvalid();
+        return validSoFar && inputCmp.get('v.validity').valid;
+    }, true);
 
+    // If we pass error checking, Continue
+    if(validItem){
+        // Create the new item
+        var newItem = component.get("v.newItem");
+        console.log("Create item: " + JSON.stringify(newItem));
+        helper.createItem(component, newItem);
+    }
     }
 })
--- force-app/main/default/aura/campingList/campingListHelper.js
+++ force-app/main/default/aura/campingList/campingListHelper.js
@@ -1,5 +1,15 @@
 ({
-    helperMethod : function() {
+    createItem : function(component, item) {
+        var campingItems = component.get("v.items");
+        var newItem = JSON.parse(JSON.stringify(newItem));
 
+        campingItems.push(newItem);
+        component.set("v.items", campingItems);
+
+        component.set("v.newItem", {
+            'sObjectType':'Camping_Item__c',
+            'Quantity__c':'0',
+            'Price__c':'0',
+            'Packed__c':false});
     }
 })
